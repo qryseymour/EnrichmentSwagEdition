4f4a69e71cfb71235a96132ea3b7b99cb768af9b
Qry <qryseymour@gmail.com>
2025-03-17 00:59:11 -0400

    Added waterframe stuff

--- kubejs/server_scripts/mod-specific/media_items.js
+++ kubejs/server_scripts/mod-specific/media_items.js
@@ -13,89 +13,89 @@ ServerEvents.recipes(e => {
         }
     ).id('immersive_paintings:painting')
     
-    // e.shaped(
-    //     Item.of('waterframes:remote', 1),
-    //     [
-    //         ' A ',
-    //         ' B ',
-    //         ' C '
-    //     ],
-    //     {
-    //         A: 'minecraft:amethyst_shard',
-    //         B: 'minecraft:torchflower',
-    //         C: 'ae2:printed_engineering_processor'
-    //     }
-    // ).id('waterframes:remote')
+    e.shaped(
+        Item.of('waterframes:remote', 1),
+        [
+            ' A ',
+            ' B ',
+            ' C '
+        ],
+        {
+            A: 'minecraft:amethyst_shard',
+            B: 'minecraft:torchflower',
+            C: 'ae2:printed_engineering_processor'
+        }
+    ).id('waterframes:remote')
     
-    // e.shaped(
-    //     Item.of('waterframes:frame', 1),
-    //     [
-    //         'ABA',
-    //         'ACE',
-    //         'ADA'
-    //     ],
-    //     {
-    //         A: 'minecraft:iron_ingot',
-    //         B: 'minecraft:torchflower',
-    //         C: 'immersive_paintings:painting',
-    //         D: 'waterframes:remote',
-    //         E: 'vinurl:custom_record'
-    //     }
-    // ).id('waterframes:frame')
+    e.shaped(
+        Item.of('waterframes:frame', 1),
+        [
+            'ABA',
+            'ACE',
+            'ADA'
+        ],
+        {
+            A: 'minecraft:iron_ingot',
+            B: 'minecraft:torchflower',
+            C: 'immersive_paintings:painting',
+            D: 'waterframes:remote',
+            E: 'vinurl:custom_record'
+        }
+    ).id('waterframes:frame')
     
-    // e.shaped(
-    //     Item.of('waterframes:projector', 1),
-    //     [
-    //         'GCE',
-    //         'FAB',
-    //         'EDE'
-    //     ],
-    //     {
-    //         A: 'immersive_paintings:painting',
-    //         B: 'botania:lens_paint',
-    //         C: 'minecraft:calibrated_sculk_sensor',
-    //         D: 'waterframes:remote',
-    //         E: 'minecraft:iron_block',
-    //         F: 'minecraft:glowstone',
-    //         G: 'vinurl:custom_record'
-    //     }
-    // ).id('waterframes:projector')
+    e.shaped(
+        Item.of('waterframes:projector', 1),
+        [
+            'GCE',
+            'FAB',
+            'EDE'
+        ],
+        {
+            A: 'immersive_paintings:painting',
+            B: 'botania:lens_paint',
+            C: 'minecraft:calibrated_sculk_sensor',
+            D: 'waterframes:remote',
+            E: 'minecraft:iron_block',
+            F: 'minecraft:glowstone',
+            G: 'vinurl:custom_record'
+        }
+    ).id('waterframes:projector')
     
-    // e.shaped(
-    //     Item.of('waterframes:tv', 1),
-    //     [
-    //         'HAF',
-    //         ' BC',
-    //         'GED'
-    //     ],
-    //     {
-    //         A: 'minecraft:sculk_sensor',
-    //         B: 'waterframes:frame',
-    //         C: 'travelersbackpack:jukebox_upgrade',
-    //         D: '#ae2:smart_cable',
-    //         E: 'ae2:color_applicator',
-    //         F: 'create:electron_tube',
-    //         G: 'create_new_age:electrical_connector',
-    //         H: 'botania:lens_paint'
-    //     }
-    // ).id('waterframes:tv')
+    e.shaped(
+        Item.of('waterframes:tv', 1),
+        [
+            'HAF',
+            ' BC',
+            'GED'
+        ],
+        {
+            A: 'minecraft:sculk_sensor',
+            B: 'waterframes:frame',
+            C: 'travelersbackpack:jukebox_upgrade',
+            D: '#ae2:smart_cable',
+            E: 'ae2:color_applicator',
+            F: 'create:electron_tube',
+            G: 'create_new_age:electrical_connector',
+            H: 'botania:lens_paint'
+        }
+    ).id('waterframes:tv')
     
-    // e.shaped(
-    //     Item.of('waterframes:big_tv', 1),
-    //     [
-    //         'HAF',
-    //         'BBC',
-    //         'GED'
-    //     ],
-    //     {
-    //         A: 'minecraft:sculk_sensor',
-    //         B: 'waterframes:frame',
-    //         C: 'travelersbackpack:jukebox_upgrade',
-    //         D: '#ae2:smart_cable',
-    //         E: 'ae2:color_applicator',
-    //         F: 'create:electron_tube',
-    //         G: 'create_new_age:electrical_connector',
-    //         H: 'botania:lens_paint'
-    //     }
-    // ).id('waterframes:big_tv')
+    e.shaped(
+        Item.of('waterframes:big_tv', 1),
+        [
+            'HAF',
+            'BBC',
+            'GED'
+        ],
+        {
+            A: 'minecraft:sculk_sensor',
+            B: 'waterframes:frame',
+            C: 'travelersbackpack:jukebox_upgrade',
+            D: '#ae2:smart_cable',
+            E: 'ae2:color_applicator',
+            F: 'create:electron_tube',
+            G: 'create_new_age:electrical_connector',
+            H: 'botania:lens_paint'
+        }
+    ).id('waterframes:big_tv')
 })
